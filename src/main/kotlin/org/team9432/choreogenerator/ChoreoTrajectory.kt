359bf4c4714da57ef6e998d10a9471decd68ea3d
Nolan L <nolan.s.locke@gmail.com>
2024-08-30 12:00:10 -0700

    replace builder from ChoreoTrajectory with normal constructor

--- src/main/kotlin/org/team9432/choreogenerator/ChoreoTrajectory.kt
+++ src/main/kotlin/org/team9432/choreogenerator/ChoreoTrajectory.kt
@@ -2,31 +2,39 @@ package org.team9432.choreogenerator
 
 import org.team9432.choreogenerator.json.JsonPath
 
-class ChoreoTrajectory private constructor(
-    private val waypoints: MutableList<ChoreoWaypoint>,
-    private val constraints: MutableList<ChoreoConstraint>,
+class ChoreoTrajectory(
     val name: String,
+    includeDefaultConstraints: Boolean = true,
     private val usesControlIntervalGuessing: Boolean = true,
     private val defaultControlIntervalCount: Int = 40,
     private val usesDefaultFieldObstacles: Boolean = true,
 ) {
+    private val waypoints = mutableListOf<ChoreoWaypoint>()
+    private val constraints = mutableListOf<ChoreoConstraint>()
 
-    companion object {
-        fun new(
-            name: String, includeDefaultConstraints: Boolean = true,
-            usesControlIntervalGuessing: Boolean = true,
-            defaultControlIntervalCount: Int = 40,
-            usesDefaultFieldObstacles: Boolean = true, block: ChoreoTrajectoryBuilder.() -> Unit,
-        ): ChoreoTrajectory {
-            val trajectory = ChoreoTrajectoryBuilder(name, usesControlIntervalGuessing, defaultControlIntervalCount, usesDefaultFieldObstacles)
-            trajectory.block()
-            if (includeDefaultConstraints) {
-                trajectory.addConstraint(InitialStopPoint)
-                trajectory.addConstraint(FinalStopPoint)
-            }
-
-            return trajectory.build()
+    init {
+        if (includeDefaultConstraints) {
+            addConstraint(InitialStopPoint)
+            addConstraint(FinalStopPoint)
         }
+    }
+
+    fun addPoseWaypoint(position: Position, stopPoint: Boolean = false) = addWaypoint(position.asPoseWaypoint(), stopPoint)
+    fun addTranslationWaypoint(position: Position, stopPoint: Boolean = false) = addWaypoint(position.asTranslationWaypoint(), stopPoint)
+
+    private fun addWaypoint(waypoint: ChoreoWaypoint, stopPoint: Boolean): Int {
+        waypoints.add(waypoint)
+        val waypointIndex = waypoints.lastIndex
+
+        if (stopPoint) {
+            addConstraint(StopPoint(waypointIndex))
+        }
+
+        return waypointIndex
+    }
+
+    fun addConstraint(constraint: ChoreoConstraint) {
+        constraints.add(constraint)
     }
 
     internal fun getJsonPath() = JsonPath(
@@ -41,34 +49,4 @@ class ChoreoTrajectory private constructor(
         eventMarkers = emptyList(),
         isTrajectoryStale = true,
     )
-
-    class ChoreoTrajectoryBuilder internal constructor(
-        private val name: String,
-        private val usesControlIntervalGuessing: Boolean = true,
-        private val defaultControlIntervalCount: Int = 40,
-        private val usesDefaultFieldObstacles: Boolean = true,
-    ) {
-        private val waypoints = mutableListOf<ChoreoWaypoint>()
-        private val constraints = mutableListOf<ChoreoConstraint>()
-
-        fun addPoseWaypoint(position: Position, stopPoint: Boolean = false) = addWaypoint(position.asPoseWaypoint(), stopPoint)
-        fun addTranslationWaypoint(position: Position, stopPoint: Boolean = false) = addWaypoint(position.asTranslationWaypoint(), stopPoint)
-
-        private fun addWaypoint(waypoint: ChoreoWaypoint, stopPoint: Boolean): Int {
-            waypoints.add(waypoint)
-            val waypointIndex = waypoints.lastIndex
-
-            if (stopPoint) {
-                addConstraint(StopPoint(waypointIndex))
-            }
-
-            return waypointIndex
-        }
-
-        fun addConstraint(constraint: ChoreoConstraint) {
-            constraints.add(constraint)
-        }
-
-        fun build() = ChoreoTrajectory(waypoints, constraints, name, usesControlIntervalGuessing, defaultControlIntervalCount, usesDefaultFieldObstacles)
-    }
 }
